88e2079d0a27b34960815d645185d0f04a8beb69
Nick <nicholastysh@gmail.com>
2019-08-07 19:10:04 -0400

    jawsDB added to connection

--- app/public/javascript/connection.js
+++ app/public/javascript/connection.js
@@ -1,12 +1,19 @@
 
 const mysql = require('mysql');
 
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'password',  // your password
-    database: 'bamazon',
-    port: 3306
-})
+var connection;
+
+    if (process.env.JAWDB_URL) {
+        connection = mysql.createConnection(process.env.JAWDB_URL)
+    } else {
+        connection = mysql.createConnection({
+            host: 'localhost',
+            user: 'root',
+            password: 'password',  // your password
+            database: 'bamazon',
+            port: 3306 
+        })
+    }
+
 
 module.exports = connection
